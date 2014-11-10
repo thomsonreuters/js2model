f654828454acede0f66f1f3a173cb2a6ccf5a7d8
Kevin Z <kevin.zimmerman@thomsonreuters.com>
2014-11-10 08:52:30 -0800

    Progress on swift support.

--- test/js2ModelSwift/js2ModelSwift/JsonModels/dependencies/JsonModelSchema.swift
+++ test/js2ModelSwift/js2ModelSwift/JsonModels/dependencies/JsonModelSchema.swift
@@ -14,8 +14,8 @@ public protocol JsonModelSerialize {
 
     class func modelSchema() -> JsonModelSchema<ModelType>
 
-    func object(forPropertyNamed propertyName:String) -> JsonInstanceMeta<ModelType, AnyObject>
-    func array(forPropertyNamed propertyName:String) ->  JsonInstanceMeta<ModelType, [AnyObject]>
+    func object(forPropertyNamed propertyName:String) -> JsonInstanceMeta<ModelType, Any>
+    func array(forPropertyNamed propertyName:String) ->  JsonInstanceMeta<ModelType, [Any]>
 
     init()
     init(jsonData:[Byte])
@@ -35,18 +35,18 @@ public protocol JsonModelSerialize {
 public class JsonPropertyMeta<M: JsonModelSerialize, T> {
     
     var getter: (M)->T?;
-    var setter: (M,T)->Void;
+    var setter: (M,T?)->Void;
     var newInstance: ()->T;
     var isArray: Bool = false;
     var newItemInstance: (()->T)?;
     
-    init(newInstance: ()->T, getter:(M)->T, setter: (M,T)->Void ) {
+    init(newInstance: ()->T, getter:(M)->T?, setter: (M,T?)->Void ) {
         self.getter = getter;
         self.setter = setter;
         self.newInstance = newInstance;
     }
 
-    init(newArrayInstance: ()->T, newItemInstance: ()->T, getter:(M)->T, setter: (M,T)->Void) {
+    init(newArrayInstance: ()->T, newItemInstance: ()->T, getter:(M)->T?, setter: (M,T?)->Void) {
         self.getter = getter;
         self.setter = setter;
         self.newInstance = newArrayInstance;
@@ -70,7 +70,7 @@ public class JsonInstanceMeta<M: JsonModelSerialize, T> {
 public class JsonModelSchema<M: JsonModelSerialize> {
 
 	var objects =  [String: JsonPropertyMeta<M, AnyObject>]();
-	var arrays =   [String: JsonPropertyMeta<M, [AnyObject]>]();
+	var arrays =   [String: JsonPropertyMeta<M, Array<Any>>]();
 	var strings =  [String: JsonPropertyMeta<M, String>]();
 	var booleans = [String: JsonPropertyMeta<M, Bool>]();
 	var integers = [String: JsonPropertyMeta<M, Int>]();
@@ -91,104 +91,68 @@ public class JsonModelSchema<M: JsonModelSerialize> {
     }
 
 
-    func object<T: JsonModelSerialize>(forPropertyNamed propertyName:(String), fromInstance instance: M, fromPropertySet propertySet:[String: JsonPropertyMeta<M, T>] ) ->
-    JsonInstanceMeta<M, T> {
-    
+    func instanceMeta<T>(forPropertyNamed propertyName:(String), fromInstance instance: M, fromPropertySet propertySet:[String: JsonPropertyMeta<M, T>] ) -> JsonInstanceMeta<M, T>? {
+        
         var propMeta = propertySet[propertyName];
-
+        
         if let pm = propMeta {
-
-            var obj = pm.getter(instance);
             
-            if let o = obj  {
-                obj = pm.newInstance();
-                pm.setter(instance, o);
+            var obj: T? = pm.getter(instance);
+            
+            if( obj == nil ) {
+                
+                if let o: T = obj  {
+                    obj = pm.newInstance();
+                    pm.setter(instance, o);
+                }
             }
             
-            return JsonInstanceMeta<M,T>(initWithInstance: obj!, propertyMeta: pm);
-
+            return JsonInstanceMeta(initWithInstance: obj!, propertyMeta: pm);
+            
         }
-        else {
-            //NSLog(@"Object for property named '%@' not found in schema %@. Adding to additionalProperties.", propertyName, NSStringFromClass([instance class]));
-            
-            var propGetter:(M)->(Any) = { (m: M) in
-                return m.value(forAdditionalProperty:propertyName);
-            }
-            
-            var propSetter:(M,Any)->(Void) = { (m: M, t: Any) in
-                m.set(t, forAdditionalProperty: propertyName);
-            }
-            
-            var newInstanceInit: ()->T = { return T() };
-            
-            var propMeta = JsonPropertyMeta(
-                newInstance: newInstanceInit,
-                getter: propGetter,
-                setter: propSetter
-                );
-            
-            var newPropInstance:T = newInstanceInit()
-            
-            propSetter(instance, newPropInstance);
-            
-            return JsonInstanceMeta<M,T>(initWithInstance: newPropInstance, propertyMeta: propMeta);
-        }
+        return nil;
     }
     
-    - (JSONInstanceMeta *)objectForPropertyNamed:(NSString *)propertyName forInstance:(id)instance {
-        
-        return [self objectForPropertyNamed:propertyName forInstance:instance from:self.objects];
-        }
-        
-        - (JSONInstanceMeta *)arrayForPropertyNamed:(NSString *)propertyName forInstance:(id)instance {
-            
-            return [self objectForPropertyNamed:propertyName forInstance:instance from:self.arrays];
-            }
-            
-            - (void)setValue:(id)val forProperty:(NSString *)propertyName forInstance:(id)instance inPropertySet:(NSDictionary *)propertySet{
-                
-                SEL setter = [self setterForProperty:propertyName from:propertySet];
-                
-                if(setter) {
-                    [instance performSelector:setter withObject:val];
-                }
-                else {
-                    NSLog(@"Setter for property named '%@' not found. Adding to additionalProperties", propertyName);
-                    [[instance additionalProperties] setValue:val forKey:propertyName];
-                }
-                }
-                
-                - (void)setString:(NSString *)val forProperty:(NSString *)propertyName forInstance:(id)instance {
-                    
-                    [self setValue:val forProperty:propertyName forInstance:instance inPropertySet:self.strings];
-                    }
-                    
-                    - (void)setNumber:(NSNumber *)val forProperty:(NSString *)propertyName forInstance:(id)instance {
-                        
-                        [self setValue:val forProperty:propertyName forInstance:instance inPropertySet:self.numbers];
-                        }
-                        
-                        - (void)setInteger:(NSNumber *)val forProperty:(NSString *)propertyName forInstance:(id)instance {
-                            
-                            [self setValue:val forProperty:propertyName forInstance:instance inPropertySet:self.integers];
-                            }
-                            
-                            - (void)setBoolean:(NSNumber *)val forProperty:(NSString *)propertyName forInstance:(id)instance {
-                                
-                                [self setValue:val forProperty:propertyName forInstance:instance inPropertySet:self.booleans];
-                                }
-                                
-                                - (void)setNullForProperty:(NSString *)propertyName forInstance:(id)instance {
-                                    
-                                    SEL setter = [self setterForProperty:propertyName];
-                                    
-                                    if(setter) {
-                                        [instance performSelector:setter withObject:nil];
-                                    }
-                                    else {
-                                        NSLog(@"Setter for null property named '%@' not found.", propertyName);
-                                    }
-                                }
-                                
+    
+    func object(forPropertyNamed propertyName:(String), fromInstance instance: M ) -> JsonInstanceMeta<M, AnyObject>? {
 
+        return instanceMeta(forPropertyNamed: propertyName, fromInstance: instance, fromPropertySet: objects);
+    }
+        
+    func array(forPropertyNamed propertyName: String, forInstance instance: M) -> JsonInstanceMeta<M, Array<Any>>?{
+        
+        return instanceMeta(forPropertyNamed: propertyName, fromInstance: instance, fromPropertySet: arrays);
+    }
+    
+    func set<T>(value val:T?, forProperty propertyName:String, forInstance instance:M, inPropertySet propertySet:[String: JsonPropertyMeta<M, T>]) {
+                
+        var propMeta = propertySet[propertyName];
+        
+        if let pm = propMeta {
+            pm.setter(instance, val);
+        }
+        else {
+            instance.set(val, forAdditionalProperty: propertyName);
+        }
+    }
+
+    func set(string val:String, forProperty propertyName:String, forInstance instance:M) {
+
+        set(value:val, forProperty:propertyName, forInstance:instance, inPropertySet:strings);
+    }
+
+    func set(number val:Double, forProperty propertyName:String, forInstance instance:M) {
+
+        set(value:val, forProperty:propertyName, forInstance:instance, inPropertySet:numbers);
+    }
+
+    func set(integer val:Int, forProperty propertyName:String, forInstance instance:M) {
+        
+        set(value:val, forProperty:propertyName, forInstance:instance, inPropertySet:integers);
+    }
+    
+    func set(boolean val:Bool, forProperty propertyName:String, forInstance instance:M) {
+        
+        set(value:val, forProperty:propertyName, forInstance:instance, inPropertySet:booleans);
+    }
 }
