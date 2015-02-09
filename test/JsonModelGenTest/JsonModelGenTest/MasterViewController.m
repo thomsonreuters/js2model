86145900c8d46af577c2731146b1f04caf622035
zim <kevin@kevinz.com>
2015-02-09 13:53:47 -0800

    Added test to profile YAJL vs NSJSONSerialization.

--- test/JsonModelGenTest/JsonModelGenTest/MasterViewController.m
+++ test/JsonModelGenTest/JsonModelGenTest/MasterViewController.m
@@ -8,6 +8,9 @@
 
 #import "MasterViewController.h"
 #import "DetailViewController.h"
+#import "TRTestData.h"
+
+const NSInteger kLoopCount = 10000;
 
 @interface MasterViewController ()
 
@@ -27,7 +30,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
+    
+    [self loadJson];
+    
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
@@ -56,6 +61,50 @@
         abort();
     }
 }
+
+-(void)loadJson {
+    
+    for (int i = 0; i < kLoopCount; i++) {
+        [self loadJsonViaJs2Model];
+    }
+    
+    for (int i = 0; i < kLoopCount; i++) {
+        [self loadJsonViaNSJSONSeriralization];
+    }
+}
+
+-(void)loadJsonViaJs2Model {
+    
+    NSString *jsonPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"js2model-test-data" ofType:@"json"];
+    
+    //    NSString *jsonFilename = [jsonPath lastPathComponent];
+    
+    NSData *jsonData = [NSData dataWithContentsOfFile:jsonPath];
+    
+    NSError *error;
+    NSArray* instances = [TRTestData testDataArrayWithJSONData:jsonData error:&error];
+    
+    NSAssert(instances, @"No instances found in JSON");
+    NSAssert(!error, @"Error parsing JSON: %@", error);
+}
+
+-(void)loadJsonViaNSJSONSeriralization {
+    
+    NSString *jsonPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"js2model-test-data" ofType:@"json"];
+    
+    //    NSString *jsonFilename = [jsonPath lastPathComponent];
+    
+    NSData *jsonData = [NSData dataWithContentsOfFile:jsonPath];
+    
+    NSError *error;
+    
+    id jsonObj = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+    
+    NSAssert(jsonObj, @"No instances found in JSON");
+    NSAssert(!error, @"Error parsing JSON: %@", error);
+}
+
+
 
 #pragma mark - Segues
 
