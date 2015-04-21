bab0cee6d2dedf8c2a487e17b098ecc52a74252d
zim <kevin@kevinz.com>
2015-04-21 10:17:06 -0700

    Fixed long-description text.

--- src/setup.py
+++ src/setup.py
@@ -6,158 +6,33 @@ ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-'''
-js2Model.py Reference
-=====================
+long_doc = '''
+Overview
+========
 
-Version 0.1 Beta DRAFT
-
-Goal
-----
-
-Make it easy to use JSON data as native class models.
+js2model makes it easy to use JSON data as native class models.
 
 JSON is ubiquitous and convenient, but parsing JSON into generic data
 structures like dictionaries and arrays precludes the use of many native
-language features. The goal of js2Model.py is to generate model classes
-and custom deserialization code from JSON schema definitions. Currently,
-only Objective-C is supported, but the source is architected in to make
-it easy to add new languages.
+language features. Js2model.py generates model classes
+and custom deserialization code from JSON schema definitions.
 
-Installation
-------------
+Languages
+=========
 
-To install js2Model:
+The script is designed to target multiple output languages. Currently, Objective-C and C++ are supported out of the box.
+Future enhancements will add the ability to add your own language templates to support any language.
 
-1. Clone the repository
-2. Install the python module
+Documentation
+=============
 
-    ::
+You can review the documentation `here <http://thomsonreuters.github.io/js2model/>`_
 
-        $cd js2Model
-        $python setup.py install
-
-Running it
-----------
-
-    .. raw:: html
-
-       <pre>
-       js2model.py [-h] [-l LANG] [--prefix PREFIX] [--rootname ROOTNAME] [-p]
-                          [-x] [-o OUTPUT] [--implements IMPLEMENTS] [--super SUPER]
-                          [--import IMPORTFILES]
-                          FILES [FILES ...]
-
-       Generate native data models from JSON.
-
-       positional arguments:
-         FILES                 JSON files input for model generation
-
-       optional arguments:
-         -h, --help            show this help message and exit
-         -l LANG, --lang LANG  language (default: objc)
-         --prefix PREFIX       prefix for class names (default: TR)
-         --rootname ROOTNAME   Class name for root schema object (default: fileName)
-         -p, --primitives      Use primitive types in favor of object wrappers
-         -x, --noadditional    Do not include additionalProperties in models
-         -o OUTPUT, --output OUTPUT
-                               Target directory of output files
-         --implements IMPLEMENTS
-                               Comma separated list of interface(s)|protocol(s)
-                               supported by the generated classes
-         --super SUPER         Comma separated list of super classes. Generated
-                               classes inherit these
-         --import IMPORTFILES  Comma separated list of files to @import
-       </pre>
-
-A simple sample
----------------
-
-Given the JSON schema definition:
-
-.. code:: json
-
-        {
-           "$schema": "http://json-schema.org/draft-04/schema#",
-           "title": "address",
-           "type": "object",
-           "properties": {
-              "street": { "type": "string" },
-              "city": { "type": "string" },
-              "county": { "type": "string" },
-              "state": { "type": "string" },
-              "zip": { "type": "string" }
-           }
-        }
-
-js2Model will generate the class source files:
-
-**address.h**
-
-.. code:: objc
-
-        @interface Address : NSObject <JSONModelSerialize>
-
-        @property(strong, nonatomic) NSString * county;
-        @property(strong, nonatomic) NSString * city;
-        @property(strong, nonatomic) NSString * state;
-        @property(strong, nonatomic) NSString * street;
-        @property(strong, nonatomic) NSString * zip;
-
-        @end
-
-**address.m**
-
-.. code:: objc
-
-        @implementation Address{
-            NSMutableDictionary *_additionalProperties;
-        }
-
-        - (instancetype)init
-        {
-            self = [super init];
-            if (self) {
-            // custom intialization code
-                     _additionalProperties = [NSMutableDictionary new];
-            }
-            return self;
-        }
-
-
-        - (instancetype) initWithJSONData:(NSData *)data
-                        error:(NSError* __autoreleasing *)error {
-            self = [self init];
-            if (self) {
-                     [TRJSONModelLoader load:self withJSONData:data error:error];
-            }
-            return self;
-        }
-
-        //
-        // Code removed for clarity
-        //
-
-        @end
-
-To deserialize JSON data into an instance of Address:
-
-.. code:: objc
-
-           NSError *error;
-
-           NSData *jsonData = [self getSomeJSONFromSomewhere];
-
-           Address *address = [Address alloc] initWithJSONData:data error:&error];
-
-           if( !error ) {
-                 NSLog(@"Street = %@", address.street);
-           }
 '''
 
 setup(
     name='js2model',
-    version='0.2.dev6',
+    version='0.2.dev7',
     packages=find_packages(),
     package_data={'tr.jsonschema': [
         'templates.objc/*.mako',
@@ -175,8 +50,7 @@ setup(
     ]},
     include_package_data=True,
     scripts=['js2model.py', 'js2model', 'ez_setup.py'],
-    keywords=['Requires: Mako, jsonpointer, jsonref, jsonschema',
-              'json',
+    keywords=['json',
               'schema',
               'jsonschema',
               'json-schema',
@@ -191,7 +65,7 @@ setup(
     author='Kevin Zimmerman',
     author_email="%s.%s@%s.%s"%('kevin', 'zimmerman', 'thomsonreuters', 'com'), # half hearted attempt to avoid spam
     description='A fine attempt to auto-generate source models + deserialization code from JSON schema definitions.',
-    long_description=__doc__,
+    long_description=long_doc,
     zip_safe=False,
     platforms='any',
     install_requires=[
